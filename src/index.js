80d3cbc4e57fec2423ba3dee8fb3be801bfc8190
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2021-08-29 15:13:01 -0700

    refactored book data being passed, added spread operator and map method

--- src/index.js
+++ src/index.js
@@ -6,36 +6,41 @@ import './index.css'
 
 const books = [ 
   {
+  id: 1,
   img: 'https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._AC_SX184_.jpg',
   title: 'This is the NEW cool title!',
   author: 'Nick Deanss'
 }, {
+  id: 2,
   img: 'https://images-na.ssl-images-amazon.com/images/I/51CPNIRSWeL._AC_SX368_.jpg',
   title: 'This is the SECOND cool title!',
   author: 'Nicky Deans'
+}, {
+  id: 3,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51uZwAA6XyL._AC_SX368_.jpg',
+  title: 'This is the Third cool title!',
+  author: 'Nickster Deans'
 },
 ]
 
-const names = ['john', 'peter', 'susan']
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>
-}) 
-console.log(newName);
+// const names = ['john', 'peter', 'susan']
+// const newNames = names.map((name) => {
+//   return <h1>{name}</h1>
+// }) 
 
 const Booklist = () => {
   return (
     <section className="booklist">
-      {newNames}
-      <Book />
-      <Book />
-      <Book />
-      <Name />
-      <Hello />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>
+      })}
     </section>
   )
 }
 
 const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
+
   return (
     <article className="book">
       <img src={img} alt='' />
@@ -43,13 +48,6 @@ const Book = ({ img, title, author }) => {
       <h4>{author}</h4>
       <p>{title}</p>
     </article>
-  )
-}
-
-const Name = () => <h1>Nick</h1>
-const Hello = () => {
-  return (
-    <h2>Hello People</h2>
   )
 }
 
