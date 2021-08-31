6e11fe31edbbb0c240dcdf56a3902aeca2529549
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2021-08-30 17:19:21 -0700

    added events and handlers

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom'
+import {data} from './books'
+import Book from './Book'
 
 // CSS
 import './index.css'
-
-const books = [ 
-  {
-  id: 1,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._AC_SX184_.jpg',
-  title: 'This is the NEW cool title!',
-  author: 'Nick Deanss'
-}, {
-  id: 2,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51CPNIRSWeL._AC_SX368_.jpg',
-  title: 'This is the SECOND cool title!',
-  author: 'Nicky Deans'
-}, {
-  id: 3,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51uZwAA6XyL._AC_SX368_.jpg',
-  title: 'This is the Third cool title!',
-  author: 'Nickster Deans'
-},
-]
 
 // const names = ['john', 'peter', 'susan']
 // const newNames = names.map((name) => {
@@ -31,23 +14,10 @@ const books = [
 const Booklist = () => {
   return (
     <section className="booklist">
-      {books.map((book) => {
+      {data.map((book) => {
         return <Book key={book.id} {...book}></Book>
       })}
     </section>
-  )
-}
-
-const Book = ({ img, title, author }) => {
-  // const { img, title, author } = props;
-
-  return (
-    <article className="book">
-      <img src={img} alt='' />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <p>{title}</p>
-    </article>
   )
 }
 
